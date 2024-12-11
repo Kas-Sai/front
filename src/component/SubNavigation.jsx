79e877104d7913e7b19adf80b903dd2c910303bb
Kas-Sai <kashyapsahil233@gmail.com>
2024-12-11 11:22:47 +0530

    update

--- src/component/SubNavigation.jsx
+++ src/component/SubNavigation.jsx
@@ -1,72 +1,34 @@
-import React from "react";
-import { 
-    Routes,
-    Route,
-    Link,
-    Outlet
-} from "react-router-dom";
+import React, { useState } from "react";
+import WoToday from "../pages/PageTransfer/components/WoToday";
+import WoHistory from "../pages/PageTransfer/components/WoHistory";
 
-import PageTransfer from "../pages/PageTransfer";
-import PageHome from "../pages/PageHome";
-import WoToday from "../woToday";
-const Navigation = ({ children }) => {
+export const SubNavigation = () => {
+    const [activeContent, setActiveContent] = useState(null);
+
+    const navItems = [
+        { id: 1, label: "Today's Ticket", component: <WoToday /> },
+        { id: 2, label: "Historical Tickets", component: <WoHistory /> },
+    ];
+
     return (
-        <div className="navbar">
-            {children}
+        <div>
+            <ul className="flex gap-4">
+                {navItems.map((item) => (
+                    <li key={item.id}>
+                        <button
+                            className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300 text-black"
+                            onClick={() => setActiveContent(item.component)}
+                        >
+                            {item.label}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <div className="mt-2 p-4 bg-yellow-400">{activeContent}</div>
         </div>
-    )
+    );
 };
 
-
-export class SubNavigation extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const navItems = [
-            {
-                id: 2,
-                label: "Transfer",
-                icon: "img/宇航员.png",
-                to: "/",
-                element: <PageTransfer />
-            },
-            {
-                id: 1,
-                label: "home page",
-                icon: "img/三球.png",
-                to: "/home",
-                element: <PageHome />
-            },
-        ]
-        let openWoToday = ()=>{
-
-        }
-        const items = this.props.items || [{id: 1, label: "current", to: ""}]
-        return (
-            <div className="subnav">
-                <ul>
-                {items.map((item, index) => (
-                    <li key={item.key} className="link">{item.label}</li>
-                ))}
-                </ul>
-                <Outlet/>
-            </div>
-        )
-    }
-}
-
-export class SubHeader extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <header className="subheader">
-                {this.props.children ? this.props.children : ""}
-            </header>
-        )
-    }
-}
+export const SubHeader = ({ children }) => {
+    return <header className="subheader">{children}</header>;
+};
